16618ca6244a11fb7e7bfbd695b3846409d890b1
Luciefifi <niyomutonilucie@gmail.com>
2023-04-17 05:06:24 +0300

    es6 classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  class Car {
+    constructor(name){
+      this.brand = name
+    }
+    present(){
+      return 'I have a ' +this.brand
+    }
+   
+  }
+  class Model extends Car{
+    constructor(name , mod){
+      super(name)
+      this.model = mod
+    }
+    show(){
+      return this.present() + ' ,  it is a ' + this.model
+    }
+      
+  }
+  const myCar = new Model('ford' , 'mustag')
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+      {/* {myCar.brand}  */}
+        {/* { myCar.present()} */}
+        { myCar.show()}
+     </h1>
     </div>
   );
 }
